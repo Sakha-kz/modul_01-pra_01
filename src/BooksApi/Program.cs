3c5936953b3eb3c7b3745f78fa08ae29e718a926
Sakha <sakhtai.erdaulet@gmail.com>
2026-09-11 04:59:53 +0500

    Render.com

--- src/BooksApi/Program.cs
+++ src/BooksApi/Program.cs
@@ -6,15 +6,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-        c.RoutePrefix = string.Empty; 
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.MapControllers();
